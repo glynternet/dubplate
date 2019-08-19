0bb7b13395e72a7f3ef528e81111937a8e86e6f9
Glyn Owen Hanmer <glynhanmer@gmail.com>
2019-08-19 14:23:55 +0200

    ♻️ alter order of temaplte replacement
    This makes creating multiple binaries a lot easier in the future

--- install.sh
+++ install.sh
@@ -6,9 +6,10 @@ appName="${1:?appName not set. $usage}"
 appRootDir="${2:?appRootDir not set\n$usage}"
 repository="${3:?repository not set\n$usage}"
 
-replaceInTmpFiles(){
-	local placeholder="${1:?placeholder not set}"
-	local replacement="${2:?replacement not set}"
+replaceInAppFiles(){
+	local appName="${1:?appName not set}"
+	local placeholder="${2:?placeholder not set}"
+	local replacement="${3:?replacement not set}"
 	find "$tmpDir" -type f -exec sed -i "s|$placeholder|$replacement|g" {} +
 }
 
@@ -18,9 +19,12 @@ cp -vr ./dubplate "$tmpDir"
 dubplateVersion="$(make --no-print-directory --directory dubplate --file dubplate.Makefile version)"
 echo Dubplate version: "$dubplateVersion"
 
-replaceInTmpFiles whiteplate "$appName"
-replaceInTmpFiles {{DUBPLATE_VERSION}} "$dubplateVersion"
-replaceInTmpFiles {{REPOSITORY}} "$repository"
-mv -v "$tmpDir/dubplate/cmd/whiteplate" "$tmpDir/dubplate/cmd/$appName"
+tmpWhiteplateDir="$tmpDir/dubplate/cmd/whiteplate"
+cp -rv "$tmpWhiteplateDir" "$tmpDir/dubplate/cmd/$appName"
+replaceInAppFiles "$appName" whiteplate "$appName"
+replaceInAppFiles "$appName" {{DUBPLATE_VERSION}} "$dubplateVersion"
+replaceInAppFiles "$appName" {{REPOSITORY}} "$repository"
+
+rm -rf "$tmpWhiteplateDir"
 
 cp -vr "$tmpDir/dubplate/." "$appRootDir/"
